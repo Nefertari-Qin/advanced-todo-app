480ccb63350f435dcac8eeae75bb4d2ad12fd1bb
Nefertari-Qin <Nefertari-Qin@users.noreply.github.com>
2019-12-28 23:36:18 -0800

    Implement part of ListTD class; also test personal GitHub account is working.

--- src/main/model/ListTD.java
+++ src/main/model/ListTD.java
@@ -1,4 +1,17 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ListTD {
+    private String description;
+    private List<TaskTD> tasks;
+
+    // Constructor: construct a list for tasks to do.
+    public ListTD(String description) {
+        this.description = description;
+        tasks = new ArrayList<>();
+    }
+
+
 }
